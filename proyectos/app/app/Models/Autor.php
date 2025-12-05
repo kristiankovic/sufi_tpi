30119fdacaf5b60f3744ae3b4a4f7d11c770f16b
kristiankovic <vv23011@ues.edu.sv>
2025-12-05 12:52:07 -0600

    feat: migraciones del autor creadas

---
+++ proyectos/app/app/Models/Autor.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Autor extends Model
+{
+    /** @use HasFactory<\Database\Factories\AutorFactory> */
+    use HasFactory;
+
+    protected $table = "autors";
+    protected $fillable = [
+        'nombre',
+        'nacionalidad'
+    ];
+}
